29f059bc55478e9f9939c84a3f8b0e972d3f1970
Görkem Özsarı <gorkem992010@gmail.com>
2021-12-23 20:43:29 +0200

    Simple game project
    Simple ball launching mobile game project.

---
+++ Ball Launcher/Assets/Scripts/BallHandler.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
+
+public class BallHandler : MonoBehaviour
+{
+    [SerializeField] private GameObject ballPrefab;
+    [SerializeField] private Rigidbody2D pivot;
+    [SerializeField] private float detachDelay;
+    [SerializeField] private float respawnDelay;
+
+    private Rigidbody2D currentBallRigidBody;
+    private SpringJoint2D currentBallSpringJoint;
+
+    private Camera mainCamera;
+    private bool isDragging;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+
+        SpawnBall();
+    }
+
+    void OnEnable()
+    {
+        EnhancedTouchSupport.Enable();
+    }
+
+    void OnDisable()
+    {
+        EnhancedTouchSupport.Disable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentBallRigidBody == null)
+        {
+            return;
+        }
+
+        if(UnityEngine.InputSystem.EnhancedTouch.Touch.activeTouches.Count == 0)
+        {
+            if(isDragging)
+            {
+                LaunchBall();
+            }
+            isDragging = false;
+            
+            return;
+        }
+        
+        isDragging = true;
+
+        currentBallRigidBody.isKinematic = true;
+        Vector2 touchPosition = new Vector2();
+        foreach(UnityEngine.InputSystem.EnhancedTouch.Touch touch in UnityEngine.InputSystem.EnhancedTouch.Touch.activeTouches)
+        {
+            touchPosition += touch.screenPosition;
+        }
+
+        touchPosition /= UnityEngine.InputSystem.EnhancedTouch.Touch.activeTouches.Count;
+    
+        Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
+
+        currentBallRigidBody.position = worldPosition;
+    }
+
+    private void SpawnBall()
+    {
+        GameObject ballInstance = Instantiate(ballPrefab, pivot.position, Quaternion.identity);
+        currentBallRigidBody = ballInstance.GetComponent<Rigidbody2D>();
+        currentBallSpringJoint = ballInstance.GetComponent<SpringJoint2D>();
+
+        currentBallSpringJoint.connectedBody = pivot;
+    }
+ 
+    private void LaunchBall()
+    {
+        currentBallRigidBody.isKinematic = false;
+        currentBallRigidBody = null;
+
+        Invoke(nameof(DetachBall), detachDelay);
+    }
+
+    private void DetachBall()
+    {
+        currentBallSpringJoint.enabled = false;
+        currentBallSpringJoint = null;
+
+        Invoke(nameof(SpawnBall), respawnDelay);
+    }
+
+}
